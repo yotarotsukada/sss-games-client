8b79643f8dd1e5e152c72773e027ec7d0856b7a7
yotarotsukada <yotarotsukada@gmail.com>
2023-01-08 23:01:29 +0900

    テキストコンポーネントを定義

---
+++ src/components/Text.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { match } from 'ts-pattern';
+import { computed } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    level?: number;
+    bold?: boolean;
+  }>(),
+  {
+    level: 4,
+  }
+);
+const fontStyle = computed(() =>
+  match(props.level)
+    .with(1, () => ['text-3xl', 'font-bold'])
+    .with(2, () => ['text-2xl', 'font-bold'])
+    .with(3, () => ['text-lg', 'font-bold'])
+    .with(4, () => ['text-base'])
+    .with(5, () => ['text-sm'])
+    .otherwise(() => {
+      throw new Error('Invalid props: text level must be inside 1-5');
+    })
+);
+</script>
+
+<template>
+  <p :class="fontStyle"><slot /></p>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
-import './style.css';
+import '@/styles/tailwind.css';
+import '@/styles/index.css';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import App from './App.vue';
