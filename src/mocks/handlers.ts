f2376ec1bdc61e35fc700783d67ca9600f7506a5
yotarotsukada <yotarotsukada@gmail.com>
2023-01-09 23:57:27 +0900

    モックを表示用データのみに簡易化

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,25 +1,23 @@
 import { rest } from 'msw';
-import { RoomResolver } from './modules/room/resolver';
-import { UserResolver } from './modules/user/resolver';
-import { CardResolver } from './modules/card/resolver';
+import { mockRoom } from './data/mockRoom';
+import { mockUser } from './data/mockUser';
 
 const { DEV_SERVICE_URL } = useConstants();
-
-const roomResolver = new RoomResolver();
-const userResolver = new UserResolver();
-const cardResolver = new CardResolver();
 
 const endpoint = (path: string) => {
   return DEV_SERVICE_URL + path;
 };
 
 export const handlers = [
-  rest.get(endpoint('/user'), userResolver.get),
+  rest.get(endpoint('/user'), (_req, res, ctx) =>
+    res(ctx.status(200), ctx.json(mockUser.one))
+  ),
 
-  rest.get(endpoint('/rooms/users/:id'), roomResolver.findByOwnerId),
-  rest.post(endpoint('/rooms'), roomResolver.create),
+  rest.get(endpoint('/rooms/users/:id'), (_req, res, ctx) =>
+    res(ctx.status(200), ctx.json(mockRoom.many))
+  ),
 
-  rest.get(endpoint('/rooms/:id'), roomResolver.findById),
-
-  rest.post(endpoint('/card'), cardResolver.create),
+  rest.get(endpoint('/rooms/:id'), (_req, res, ctx) =>
+    res(ctx.status(200), ctx.json(mockRoom.one))
+  ),
 ];
--- src/mocks/lib/random.ts
+++
@@ -1,3 +0,0 @@
-export const randBetween = (num: number) => {
-  return Math.floor(Math.random() * num);
-};
